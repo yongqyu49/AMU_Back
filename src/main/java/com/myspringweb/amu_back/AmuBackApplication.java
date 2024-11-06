f8e0be7c619a5863730a2cfb8032808e7e005e14
yongqyu49 <cyk728@naver.com>
2024-11-07 02:58:00 +0900

    Init

---
+++ src/main/java/com/myspringweb/amu_back/AmuBackApplication.java
@@ -0,0 +1,13 @@
+package com.myspringweb.amu_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AmuBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AmuBackApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/myspringweb/amu_back/AmuBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myspringweb.amu_back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AmuBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
