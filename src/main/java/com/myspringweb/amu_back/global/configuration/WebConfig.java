cbd07e0397e539b25393b0b0f05c55aef3645c7a
lsw0798 <swzzangr7890@gmail.com>
2024-11-15 01:11:26 +0900

    분기 생성

--- src/main/java/com/myspringweb/amu_back/global/configuration/WebConfig.java
+++ src/main/java/com/myspringweb/amu_back/global/configuration/WebConfig.java
@@ -4,11 +4,16 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import java.util.logging.Logger;
+
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
+    private static final Logger logger = Logger.getLogger(WebConfig.class.getName());
+
     @Override
     public void addCorsMappings(CorsRegistry registry) {
+        logger.info("CORS 연결 시작");
         registry.addMapping("/**").allowedOrigins("http://localhost:3000"); // React의 URL
     }
 }
